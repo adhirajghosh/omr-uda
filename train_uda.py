2604dae2ce35608b3f0b637029e56520b6a26038
Adhiraj Ghosh <adhirajghosh1998@gmail.com>
2021-10-12 00:04:34 +0200

    Update train_uda.py

--- train_uda.py
+++ train_uda.py
@@ -13,10 +13,16 @@ from src.data import LoadDataset
 from src.ufdn import LoadModel
 from src.util import interpolate_vae, vae_loss
 
-from tensorboardX import SummaryWriter 
+from tensorboardX import SummaryWriter
 from torchvision.transforms import *
+from transform import train_transforms
+import wandb
 
-# Load config file for experiment
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+wandb.init(project="uda", entity="adhirajghosh")
 cudnn.benchmark = True
 config_path = 'config/uda_example.yaml'
 conf = yaml.load(open(config_path,'r'))
@@ -53,10 +59,12 @@ batch_size = conf['trainer']['batch_size']
 shuffle_source = conf['exp_setting']['shuffle_source']
 shuffle_target = conf['exp_setting']['shuffle_target']
 
+#transforms = train_transforms(img_size, img_size)
 transforms = Compose([
-        Resize((512, 512)),
+        RandomCrop((32)),
         ToTensor()
     ])
+
 
 src_loader = LoadDataset(src_domain,data_root,batch_size,'train',shuffle=shuffle_source, transform=transforms)
 tgt_loader = LoadDataset(tgt_domain,data_root,batch_size,'train',shuffle=shuffle_target, transform=transforms)
@@ -66,8 +74,7 @@ tgt_test = LoadDataset(tgt_domain,data_root,100,'test', transform=transforms)
 
 print("Made it so far.")
 
-
-for (d1),(d2) in zip(src_test,tgt_test):
+for (d1,_),(d2) in zip(src_test,tgt_test):
     if d1[0].shape[0] == 1:
         src_test_sample = d1[0]. sclone().repeat(3,1,1)*2-1
     else:
@@ -78,8 +85,10 @@ for (d1),(d2) in zip(src_test,tgt_test):
         tgt_test_sample = d2[0]*2-1
     break
 
-# Load Model
+
+
 enc_dim = conf['model']['vae']['encoder'][-1][1]
+print(enc_dim)
 code_dim = conf['model']['vae']['code_dim']
 vae_learning_rate = conf['model']['vae']['lr']
 vae_betas = tuple(conf['model']['vae']['betas'])
@@ -87,6 +96,7 @@ df_learning_rate = conf['model']['D_feat']['lr']
 df_betas = tuple(conf['model']['D_feat']['betas'])
 
 vae = LoadModel('vae',conf['model']['vae'],img_size,img_depth)
+print(vae)
 d_feat = LoadModel('nn',conf['model']['D_feat'],img_size,enc_dim)
 d_digit = LoadModel('nn',conf['model']['D_digit'],img_size,enc_dim)
 
@@ -133,13 +143,12 @@ for k in trainer_conf['lambda'].keys():
     loss_lambda[k]['inc'] = (final-init)/step
     loss_lambda[k]['final'] = final
 
-
-
-
-while global_step < trainer_conf['total_step']:    
-    for (src_img),(tgt_img) in zip(src_loader,tgt_loader):
-        # print(src_img)
-        # print(src_label)
+#while global_step < trainer_conf['total_step']:
+for global_step in range(trainer_conf['total_step']):
+    print("Epoch ", global_step+1)
+    count = 0
+    for (src_img, src_label),(tgt_img) in zip(src_loader,tgt_loader):
+        count = count+1
         # Make all images 3-channel and perform augmentation if specified
         if src_img.shape[1] == 1:
             src_img = src_img.repeat(1,3,1,1)
@@ -152,137 +161,195 @@ while global_step < trainer_conf['total_step']:
 
         input_img = torch.cat([src_img,tgt_img],dim=0)
         input_img = Variable((input_img*2-1).cuda(),requires_grad=False)
+        src_label = Variable((src_label).cuda(), requires_grad=False)
 
-        
-        # Only using label from src domain
-        # tgt_label.fill_(-1)
-        # digit_label = Variable(torch.cat([src_label,tgt_label],dim=0).cuda(),requires_grad=False)
-                
-        
-        # Train Feature Discriminator
+        tgt_label = src_label
+        tgt_label.fill_(-1)
+        digit_label = Variable(torch.cat([src_label, tgt_label], dim=0).cuda(), requires_grad=False)
+
         opt_df.zero_grad()
-        
-        enc_x = vae(input_img,return_enc=True).detach()
-        print(enc_x.shape)
+
+        enc_x = vae(input_img, return_enc=True).detach()
+        #print(enc_x.shape)
         domain_pred = d_feat(enc_x)
 
-        df_loss = clf_loss(domain_pred,domain_code)
+        df_loss = clf_loss(domain_pred, domain_code)
         df_loss.backward()
-     
+
         opt_df.step()
-        
 
         # Train VAE
         opt_vae.zero_grad()
-        
+
         ### Reconstruction Phase
-        recon_batch, mu, logvar = vae(input_img,insert_attrs = domain_code)
-        # print(recon_batch)
-        # print(mu)
-        # print(logvar)
-        mse,kl = vae_loss(recon_batch.view(batch_size,-1), input_img.view(batch_size,-1), mu, logvar, reconstruct_loss)
+        recon_batch, mu, logvar = vae(input_img, insert_attrs=domain_code)
 
-        recon_loss = (loss_lambda['pix_recon']['cur']*mse+loss_lambda['kl']['cur']*kl)
+        mse, kl = vae_loss(recon_batch.view(batch_size, -1), input_img.view(batch_size, -1), mu, logvar,
+                           reconstruct_loss)
+
+        recon_loss = (loss_lambda['pix_recon']['cur'] * mse + loss_lambda['kl']['cur'] * kl)
         recon_loss.backward()
-        # print(mse)
-        # print(kl)
-        
-        ### Adversarial Phase       
-        enc_x = vae(input_img,return_enc=True)
+
+
+        ### Adversarial Phase
+        enc_x = vae(input_img, return_enc=True)
         domain_pred = d_feat(enc_x)
-        domain_loss = clf_loss(domain_pred,invert_code)            
+        domain_loss = clf_loss(domain_pred, invert_code)
 
-        adv_loss = loss_lambda['feat_domain']['cur']*domain_loss
+        adv_loss = loss_lambda['feat_domain']['cur'] * domain_loss
         adv_loss.backward()
-            
-        
-        ### Digit Phase
-        # enc_x = vae(input_img,return_enc=True)
-        # digit_pred = d_digit(enc_x)
-        # dg_loss = digit_clf_loss(digit_pred,digit_label)
-        # dg_loss.backward()
-        
-        
+
         opt_vae.step()
-        
-        
-        # End of step        
+
+        # End of step
         global_step += 1
-        
+
         # Records
-        if trainer_conf['save_log'] and (global_step % trainer_conf['verbose_step'] ==0):
+        if trainer_conf['save_log'] and (global_step % trainer_conf['verbose_step'] == 0):
             writer.add_scalar('tMSE', mse.data, global_step)
-            writer.add_scalar('KL',  kl.data, global_step)
+            writer.add_scalar('KL', kl.data, global_step)
 
-            writer.add_scalars('Domain_loss', {'VAE':adv_loss.data,
-                                              'D':df_loss.data}, global_step)
-            #writer.add_scalar('Digit_loss', dg_loss.data, global_step)
+            writer.add_scalars('Domain_loss', {'VAE': adv_loss.data,
+                                               'D': df_loss.data}, global_step)
+            print(f"VAE Loss: {adv_loss.data}, tMSE: {mse.data}, KL: {kl.data}")
 
-        print(f"Domain Loss: {adv_loss.data}")
+        #print(f"Domain Loss: {adv_loss.data}")
 
-
-            
         # update lambda
         for k in loss_lambda.keys():
-            if loss_lambda[k]['inc']*loss_lambda[k]['cur'] < loss_lambda[k]['inc']*loss_lambda[k]['final']:    
-                loss_lambda[k]['cur'] += loss_lambda[k]['inc'] 
+            if loss_lambda[k]['inc'] * loss_lambda[k]['cur'] < loss_lambda[k]['inc'] * loss_lambda[k]['final']:
+                loss_lambda[k]['cur'] += loss_lambda[k]['inc']
 
-
-        if global_step%trainer_conf['checkpoint_step']==0 and trainer_conf['save_checkpoint'] and not trainer_conf['save_best_only']:
-            torch.save(vae,model_path.format(global_step)+'.vae')
-            torch.save(d_digit,model_path.format(global_step)+'.dnet')
-
+        if global_step % trainer_conf['checkpoint_step'] == 0 and trainer_conf['save_checkpoint'] and not trainer_conf[
+            'save_best_only']:
+            torch.save(vae, model_path.format(global_step) + '.vae')
+            torch.save(d_digit, model_path.format(global_step) + '.dnet')
 
         ### Show result
-        if global_step% trainer_conf['plot_step'] ==0:
+        if global_step % trainer_conf['plot_step'] == 0:
             vae.eval()
             d_digit.eval()
 
             # Reconstruct
-            tmp = interpolate_vae(vae, src_test_sample, tgt_test_sample,attr_max=1.0,attr_dim=code_dim)
-            fig1 = (tmp+1)/2
+            tmp = interpolate_vae(vae, src_test_sample, tgt_test_sample, attr_max=1.0, attr_dim=code_dim)
+            fig1 = (tmp + 1) / 2
 
-            # UDA test
-            source_acc = []
-            target_acc = []
-            
-            for test_batch,test_label in src_test:
-                if test_batch.shape[1] == 1:
-                    test_batch = test_batch.repeat(1,3,1,1)
-                test_batch = Variable(test_batch.cuda())
-                label_pred = d_digit(vae((test_batch*2-1),return_enc=True))
-                acc = float(sum(np.argmax(label_pred.cpu().data.numpy(),axis=-1)==test_label.numpy().reshape(-1)))/len(test_label)
-                
-                source_acc.append(acc)
-                
-            for test_batch,test_label in tgt_test:
-                if test_batch.shape[1] == 1:
-                    test_batch = test_batch.repeat(1,3,1,1)
-                test_batch = Variable(test_batch.cuda())
-                label_pred = d_digit(vae((test_batch*2-1),return_enc=True))
-                
-                acc = float(sum(np.argmax(label_pred.cpu().data.numpy(),axis=-1)==test_label.numpy().reshape(-1)))/len(test_label)
-                target_acc.append(acc)
-                
-                
-            source_acc = sum(source_acc)/len(source_acc)
-            target_acc = sum(target_acc)/len(target_acc)
-
-            if target_acc > best_acc:
-                best_acc = target_acc
-                if trainer_conf['save_best_only'] and trainer_conf['save_checkpoint']:
-                    with open(model_path+'acc.txt','w') as f:
-                        f.write(str(global_step)+'\t'+str(best_acc)+'\n')
-                    torch.save(vae,model_path+'vae')
-                    torch.save(d_digit,model_path+'dnet')
-            
-            
-            if trainer_conf['save_log']:
-                writer.add_scalars('Accuracy',{'source':source_acc,
-                                               'target':target_acc}, global_step)
-            print(f"Source Accuracy: {source_acc}, Target Accuracy: {target_acc}")
+            #print(f"Source Accuracy: {source_acc}, Target Accuracy: {target_acc}")
             if trainer_conf['save_fig']:
-                writer.add_image('interpolate', torch.FloatTensor(np.transpose(fig1,(2,0,1))), global_step)
-                
+                writer.add_image('interpolate', torch.FloatTensor(np.transpose(fig1, (2, 0, 1))), global_step)
+
             vae.train()
             d_digit.train()
+
+        # opt_df.zero_grad()
+        #
+        # enc_x = vae(input_img, return_enc=True).detach()
+        # domain_pred = d_feat(enc_x)
+        #
+        # df_loss = clf_loss(domain_pred, domain_code)
+        # df_loss.backward()
+        #
+        # opt_df.step()
+        #
+        # # Train VAE
+        # opt_vae.zero_grad()
+        #
+        # ### Reconstruction Phase
+        # recon_batch, mu, logvar = vae(input_img, insert_attrs=domain_code)
+        # mse, kl = vae_loss(recon_batch.view(batch_size, -1), input_img.view(batch_size, -1), mu, logvar,
+        #                    reconstruct_loss)
+        # recon_loss = (loss_lambda['pix_recon']['cur'] * mse + loss_lambda['kl']['cur'] * kl)
+        # recon_loss.backward()
+        #
+        # ### Adversarial Phase
+        # enc_x = vae(input_img, return_enc=True)
+        # domain_pred = d_feat(enc_x)
+        # domain_loss = clf_loss(domain_pred, invert_code)
+        #
+        # adv_loss = loss_lambda['feat_domain']['cur'] * domain_loss
+        # adv_loss.backward()
+        #
+        # ### Digit Phase
+        # enc_x = vae(input_img, return_enc=True)
+        # digit_pred = d_digit(enc_x)
+        # dg_loss = digit_clf_loss(digit_pred, digit_label)
+        # dg_loss.backward()
+        #
+        # opt_vae.step()
+        #
+        # # End of step
+        # global_step += 1
+        #
+        # # Records
+        # if trainer_conf['save_log'] and (count % trainer_conf['verbose_step'] == 0):
+        #     writer.add_scalar('MSE', mse.data[0], global_step)
+        #     writer.add_scalar('KL', kl.data[0], global_step)
+        #
+        #     writer.add_scalars('Domain_loss', {'VAE': adv_loss.data[0],
+        #                                        'D': df_loss.data[0]}, global_step)
+        #     writer.add_scalar('Digit_loss', dg_loss.data[0], global_step)
+        #
+        #     print(f"VAE Loss: {adv_loss.data}, Recon Loss: {recon_loss}, tMSE: {mse.data}, KL: {kl.data}")
+        #
+        # # update lambda
+        # for k in loss_lambda.keys():
+        #     if loss_lambda[k]['inc'] * loss_lambda[k]['cur'] < loss_lambda[k]['inc'] * loss_lambda[k]['final']:
+        #         loss_lambda[k]['cur'] += loss_lambda[k]['inc']
+        #
+        # if global_step % trainer_conf['checkpoint_step'] == 0 and trainer_conf['save_checkpoint'] and not trainer_conf[
+        #     'save_best_only']:
+        #     print("Saving model")
+        #     torch.save(vae, model_path.format(global_step) + 'vae.pt')
+        #     torch.save(d_digit, model_path.format(global_step) + 'dnet.pt')
+        #
+        # ### Show result
+        # if global_step % trainer_conf['plot_step'] == 0:
+        #     vae.eval()
+        #     d_digit.eval()
+        #
+        #     # Reconstruct
+        #     tmp = interpolate_vae(vae, src_test_sample, tgt_test_sample, attr_max=1.0, attr_dim=code_dim)
+        #     fig1 = (tmp + 1) / 2
+        #
+        #     # UDA test
+        #     source_acc = []
+        #     target_acc = []
+        #
+        #     for test_batch, test_label in src_test:
+        #         if test_batch.shape[1] == 1:
+        #             test_batch = test_batch.repeat(1, 3, 1, 1)
+        #         test_batch = Variable(test_batch.cuda())
+        #         label_pred = d_digit(vae((test_batch * 2 - 1), return_enc=True))
+        #         acc = float(
+        #             sum(np.argmax(label_pred.cpu().data.numpy(), axis=-1) == test_label.numpy().reshape(-1))) / len(
+        #             test_label)
+        #
+        #         source_acc.append(acc)
+        #
+        #     # for test_batch, test_label in tgt_test:
+        #     #     if test_batch.shape[1] == 1:
+        #     #         test_batch = test_batch.repeat(1, 3, 1, 1)
+        #     #     test_batch = Variable(test_batch.cuda())
+        #     #     label_pred = d_digit(vae((test_batch * 2 - 1), return_enc=True))
+        #     #
+        #     #     acc = float(
+        #     #         sum(np.argmax(label_pred.cpu().data.numpy(), axis=-1) == test_label.numpy().reshape(-1))) / len(
+        #     #         test_label)
+        #     #     target_acc.append(acc)
+        #
+        #     source_acc = sum(source_acc) / len(source_acc)
+        #     # target_acc = sum(target_acc) / len(target_acc)
+        #
+        #     if trainer_conf['save_best_only'] and trainer_conf['save_checkpoint']:
+        #
+        #         torch.save(vae, model_path + 'vae.pt')
+        #         torch.save(d_digit, model_path + 'dnet.pt')
+        #
+        #     if trainer_conf['save_log']:
+        #         writer.add_scalars('Accuracy', {'source': source_acc}, global_step)
+        #     if trainer_conf['save_fig']:
+        #         writer.add_image('interpolate', torch.FloatTensor(np.transpose(fig1, (2, 0, 1))), global_step)
+        #
+        #     vae.train()
+        #     d_digit.train()
+
